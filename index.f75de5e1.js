985ecbd7e816b0c4021455c9a3cd9d7d39db1699
Alina Kabanets <kabanets.dolls@gmail.com>
2024-01-02 12:54:18 +0000

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    "Drink": {
+        "Wine": {},
+        "Schnaps": {}
+    },
+    "Fruit": {
+        "Red": {
+            "Cherry": {},
+            "Strawberry": {}
+        },
+        "Yellow": {
+            "Banana": {},
+            "Pineapple": {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    element.append(list);
+    for(const item in data){
+        const li = document.createElement("li");
+        li.textContent = item;
+        list.append(li);
+        if (Object.keys(data[item]).length > 0) createTree(li, data[item]);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
